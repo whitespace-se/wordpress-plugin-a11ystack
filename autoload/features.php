b2553ef6de9b62a0da62f068d6324e5c828910f6
Fredrik Johansson <jeanfredrik@gmail.com>
2022-05-05 13:36:51 +0200

    feat: Add feature flag for feedback module

---
+++ autoload/features.php
@@ -0,0 +1,12 @@
+<?php
+
+function whitespace_a11ystack_feature_enabled($feature) {
+  $constant = "WHITESPACE_A11YSTACK_ENABLE_" . strtoupper($feature);
+  if (defined($constant)) {
+    $value = constant($constant);
+    if (!is_null($value)) {
+      return (bool) $value;
+    }
+  }
+  return get_option("whitespace_a11ystack_feature_" . $feature);
+}
--- autoload/mod-feedback.php
+++ autoload/mod-feedback.php
@@ -1,6 +1,8 @@
 <?php
 
 add_filter("Modularity/Modules", function ($modules) {
-  $modules[WHITESPACE_A11YSTACK_PATH . "/src/Modularity"] = "ModFeedback";
+  if (whitespace_a11ystack_feature_enabled("feedback_module")) {
+    $modules[WHITESPACE_A11YSTACK_PATH . "/src/Modularity"] = "ModFeedback";
+  }
   return $modules;
 });
