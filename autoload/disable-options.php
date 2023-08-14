6ecf2fc5e0e5e3d2dcf376f81b44c9af1f99a369
Fredrik Johansson <jeanfredrik@gmail.com>
2023-08-14 13:45:10 +0200

    feat: Disable more options

--- autoload/disable-options.php
+++ autoload/disable-options.php
@@ -12,6 +12,16 @@ add_action(
     acf_remove_local_field("field_56a0a7e36365b");
     // Color scheme
     acf_remove_local_field("field_5a9946401d638");
+    // Logotype
+    acf_remove_local_field_group("group_56a0f1f7826dd");
+    // Cookie consent
+    acf_remove_local_field_group("group_56bc6b6466df1");
+    // Favicon
+    acf_remove_local_field_group("group_56cc39aba8782");
+    // GDPR
+    acf_remove_local_field_group("group_5ac334058cc33");
+    // 2.0 Enabler
+    acf_remove_local_field_group("group_5aa14b41551ae");
   },
   20,
 );
