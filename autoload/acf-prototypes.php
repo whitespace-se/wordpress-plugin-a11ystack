5fc54c8e40b57d04851cc67fa8f27e13224aa9dd
Fredrik Johansson <jeanfredrik@gmail.com>
2023-11-15 11:52:19 +0100

    feat: Add variant to billboard links

--- autoload/acf-prototypes.php
+++ autoload/acf-prototypes.php
@@ -1,6 +1,9 @@
 <?php
 
 function whitespace_a11ystack_acf_links_field($extra = [], $options = []) {
+  $variants = apply_filters("whitespace_a11ystack_acf_links_field_variants", [
+    "auto" => __("Auto", "whitespace-a11ystack"),
+  ]);
   $field = [
     "key" => "field_mod_billboard_links",
     "label" => __("Buttons", "whitespace-a11ystack"),
@@ -16,6 +19,17 @@ function whitespace_a11ystack_acf_links_field($extra = [], $options = []) {
         "name" => "link",
         "type" => "link",
         "required" => 1,
+      ],
+      [
+        "key" => "field_mod_billboard_links_variant",
+        "label" => __("Style", "whitespace-a11ystack"),
+        "name" => "variant",
+        "type" => "select",
+        "choices" => $variants,
+        "default_value" => array_keys($variants)[0],
+        "wrapper" => [
+          "width" => "20",
+        ],
       ],
     ],
   ];
